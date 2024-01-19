0b551921354206926c9fe638c30ff63688686b00
Gabriel Otávio de Barros <156124251+gabrielbarros2002@users.noreply.github.com>
2024-01-19 17:58:28 -0300

    Exercicios

--- Módulo 3/ex007 - Strings/script/javascript.js
+++ Módulo 3/ex007 - Strings/script/javascript.js
@@ -6,13 +6,13 @@ console.log(str);
 console.log(str.slice(-6));
 console.log(str.slice(6));
 console.log(str.slice(3, 4));
-console.log(str.substr(3, 3))
-console.log(str.substring(3, 5))
-console.log(str.replace('de Barros', 'Schuhmacher'))
+console.log(str.substr(3, 3));
+console.log(str.substring(3, 5));
+console.log(str.replace('de Barros', 'Schuhmacher'));
 console.log(str.toUpperCase());
-console.log(str.toLocaleLowerCase())
-console.log(str.concat(' Schuhmacher'))
-console.log(str + ' Schuhmacher')
-console.log(str.charAt(3))
-console.log(str[3])
-console.log(str.split(' '))
+console.log(str.toLocaleLowerCase());
+console.log(str.concat(' Schuhmacher'));
+console.log(str + ' Schuhmacher');
+console.log(str.charAt(3));
+console.log(str[3]);
+console.log(str.split(' '));
---
+++ Módulo 3/ex008 - Numbers/script/javascript.js
@@ -0,0 +1,18 @@
+let str = '     Gabriel Otávio de Barros     ';
+
+str = str.trim()
+
+console.log(str);
+console.log(str.slice(-6));
+console.log(str.slice(6));
+console.log(str.slice(3, 4));
+console.log(str.substr(3, 3))
+console.log(str.substring(3, 5))
+console.log(str.replace('de Barros', 'Schuhmacher'))
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase())
+console.log(str.concat(' Schuhmacher'))
+console.log(str + ' Schuhmacher')
+console.log(str.charAt(3))
+console.log(str[3])
+console.log(str.split(' '))
