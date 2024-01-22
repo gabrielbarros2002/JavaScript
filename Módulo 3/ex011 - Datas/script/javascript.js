42f7e2ba7d10cfa13412138de5d51837e2d83059
Gabriel Otávio de Barros <156124251+gabrielbarros2002@users.noreply.github.com>
2024-01-22 17:46:27 -0300

    ex011 - datas

---
+++ Módulo 3/ex011 - Datas/script/javascript.js
@@ -0,0 +1,21 @@
+let d = new Date('2024-01-22 16:45');
+
+console.log(d);
+
+// Formatações de Datas
+console.log(d.toDateString());
+console.log(d.toUTCString());
+console.log(d.toString());
+console.log(d.toDateString());
+
+// Retornar o Ano
+console.log(d.getFullYear());
+
+// Retornar o Dia da Semana (Começando no 0 - Domingo)
+console.log(d.getDay());
+
+// Retornar o Dia
+console.log(d.getDate());
+
+// Retornar os Segundos 
+console.log()
