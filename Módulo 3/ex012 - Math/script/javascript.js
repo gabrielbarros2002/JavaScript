99ba47d0010f908deddb803586ff0b53476b7ae5
Gabriel Otávio de Barros <156124251+gabrielbarros2002@users.noreply.github.com>
2024-01-22 18:00:03 -0300

    ex012

---
+++ Módulo 3/ex012 - Math/script/javascript.js
@@ -0,0 +1,25 @@
+let n = 3.54
+
+// Valor de PI
+console.log(Math.PI);
+
+// Arredonda o valor
+console.log(Math.round(n));
+
+// Arredonda o valor para baixo
+console.log(Math.floor(n));
+
+// Arredonda o valor para cima
+console.log(Math.ceil(n));
+
+// Valor Absoluto 
+console.log(Math.abs(n));
+
+// Valor mínimo entre vários números
+console.log(Math.min());
+
+// Valor máximo entre vários números
+console.log(Math.max());
+
+// Gerar valor aleatório entre 0 e 1
+console.log(Math.random())
